129dee185554933c8d9b3321b08974a18410f321
BojkaHD <bojkahd@gmail.com>
2025-05-31 00:28:11 +0200

    ✨ Add App-to-User Test Payment Endpoint

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
 import { createClient } from '@supabase/supabase-js';
 
